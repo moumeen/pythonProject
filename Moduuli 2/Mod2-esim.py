b2f05030431008392c4eb3ddc09373a0808bcd11
moumeen <moumen.flih@metropolia.fi>
2024-08-22 15:04:55 +0300

    lisätty toinen tulostus

---
+++ Moduuli 2/Mod2-esim.py
@@ -0,0 +1,31 @@
+import math
+
+
+# input = syötteen lukeminen
+name = input ("anna nimesi:")
+# name = "matti" merkkijono (string)
+# \ on esape-merkki, jolla voi tuottaa esim. tabin tai rivinvaihto
+print ("moi \t" + name)   # moi + matti
+
+
+#age = "7"
+age = input (" Anna ikäsi ")
+print(" ikäsi on " + age)
+
+age = int(age) + 1
+
+age_string = str(age) # muutetaan int --> string, esim 8 --> "8"
+print(" Ikäsi on vuoden päästä "  + age_string)
+age = age + 1
+# toinen tapa, tehdään tyyppimuunnos vain tarvittavan kohtaan
+print(" ikäsi on kahden vuoden päästä " + str(age))
+
+#käyttäjän pituus merkintä
+height = input( "anna pituus: ")
+height= float(height)
+height = height + 0.1
+# tulos f-strringin muodossa, ei tarvita str ()-funktiota
+print(f" Nimi:{name}, Ikä: {age}, pituus: {height:.2f} metriä.")
+
+
+print(math.pi)
---
+++ Moduuli 2/teht 2.py
@@ -0,0 +1,7 @@
+import math
+
+
+r =  float(input(" Anna ympyrän säde (m): "))
+
+area = math.pi * r * r
+print(f"Ympyrän, jonka säde on {r}, pinta-ala on {area} neliömetriä ")
