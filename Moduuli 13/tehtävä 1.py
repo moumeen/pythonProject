fbbe350915ffdbe68fd9befd72b2e84040f41025
moumeen <moumen.flih@metropolia.fi>
2024-11-12 15:01:54 +0200

    lisätty toinen tulostus

--- Moduuli 13/tehtävä 1.py
+++ Moduuli 13/tehtävä 1.py
@@ -1,10 +1,23 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+import math
 
 app = Flask(__name__)
-@app.route('/')
-def hello():
 
-    return "Terve maailma"
+def onko_alkuluku(luku):
+    if luku <= 1:
+        return False
+    for i in range(2, int(math.sqrt(luku)) + 1):
+        if luku % i == 0:
+            return False
+    return True
+
+@app.route('/alkuluku/<int:luku>', methods=['GET'])
+def tarkista_alkuluku(luku):
+    tulos = {
+        "Number": luku,
+        "isPrime": onko_alkuluku(luku)
+    }
+    return jsonify(tulos)
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    app.run(debug=True, port=3000)
---
+++ Moduuli 13/tehtävä 2.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+
+
+def create_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="salasana",
+        database="flight_game",  # Tietokannan nimi
+        autocommit=True
+    )
+
+
+def get_airports():
+    conn = create_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
+             FROM airport
+             WHERE continent = 'EU' 
+             AND type='large_airport'
+             ORDER BY RAND()
+             LIMIT 30;"""
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+@app.route('/kenttä/<icao_koodi>', methods=['GET'])
+def hae_lentokentta_info(icao_koodi):
+    conn = create_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    sql = """SELECT iso_country, ident, name, municipality
+             FROM airport
+             WHERE ident = %s;"""
+
+    cursor.execute(sql, (icao_koodi.upper(),))
+    kentta = cursor.fetchone()
+    conn.close()
+
+    if kentta:
+        return jsonify(kentta)
+    else:
+        return jsonify({"error": "Lentokenttää ei löytynyt"}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
